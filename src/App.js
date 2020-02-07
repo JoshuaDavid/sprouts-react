876a7cf8739e0c98b8b9464f639399d5f4328d9a
Joshua David <Joshua@Joshua-David.net>
2020-02-07 16:53:35 -0300

    mv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import SpringySim from './springysim.js';
+import SpringySim from './SpringySim.js';
 
 class App extends React.Component {
     render() {
